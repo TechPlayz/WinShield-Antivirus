a68e7a954519f8f5504c4c9d06a708ca63f22011
TechPlayz <prabhuppratham@gmail.com>
2021-07-31 20:20:34 +0530

    worked on it

--- WinShield Antivirus/Form1.Designer.cs
+++ WinShield Antivirus/Form1.Designer.cs
@@ -30,12 +30,108 @@ namespace WinShield_Antivirus
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.scan = new System.Windows.Forms.Button();
+            this.Progress_Indicator = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Patience = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.scanned_file_heading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1, 34);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(798, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // scan
+            // 
+            this.scan.Location = new System.Drawing.Point(324, 105);
+            this.scan.Name = "scan";
+            this.scan.Size = new System.Drawing.Size(128, 23);
+            this.scan.TabIndex = 1;
+            this.scan.Text = "Scan for viruses";
+            this.scan.UseVisualStyleBackColor = true;
+            this.scan.Click += new System.EventHandler(this.scan_Click);
+            // 
+            // Progress_Indicator
+            // 
+            this.Progress_Indicator.AutoSize = true;
+            this.Progress_Indicator.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Progress_Indicator.Location = new System.Drawing.Point(13, 15);
+            this.Progress_Indicator.Name = "Progress_Indicator";
+            this.Progress_Indicator.Size = new System.Drawing.Size(51, 13);
+            this.Progress_Indicator.TabIndex = 2;
+            this.Progress_Indicator.Text = "Progress:";
+            this.Progress_Indicator.Click += new System.EventHandler(this.Progress_Indicator_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Patience
+            // 
+            this.Patience.AutoSize = true;
+            this.Patience.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.Patience.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Patience.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Patience.ForeColor = System.Drawing.Color.Crimson;
+            this.Patience.Location = new System.Drawing.Point(221, 158);
+            this.Patience.Name = "Patience";
+            this.Patience.Size = new System.Drawing.Size(363, 25);
+            this.Patience.TabIndex = 4;
+            this.Patience.Text = "Please be patient until scan ends";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(1, 105);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(174, 140);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // scanned_file_heading
+            // 
+            this.scanned_file_heading.AutoSize = true;
+            this.scanned_file_heading.Font = new System.Drawing.Font("Sitka Subheading", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scanned_file_heading.Location = new System.Drawing.Point(12, 79);
+            this.scanned_file_heading.Name = "scanned_file_heading";
+            this.scanned_file_heading.Size = new System.Drawing.Size(112, 23);
+            this.scanned_file_heading.TabIndex = 6;
+            this.scanned_file_heading.Text = "Scanned Files:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.scanned_file_heading);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Patience);
+            this.Controls.Add(this.Progress_Indicator);
+            this.Controls.Add(this.scan);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "WinShield Antivirus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button scan;
+        private System.Windows.Forms.Label Progress_Indicator;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Patience;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label scanned_file_heading;
     }
 }
 
--- WinShield Antivirus/Form1.cs
+++ WinShield Antivirus/Form1.cs
@@ -16,5 +16,49 @@ namespace WinShield_Antivirus
         {
             InitializeComponent();
         }
+
+        private void scan_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Interval = 1000;
+            progressBar1.Maximum = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            
+        }
+
+        private void Progress_Indicator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value != 100)
+            {
+                progressBar1.Value++;
+                
+                //lst.Add("C:\\Windows\\System32\\shell32.dll\n");
+                /*
+                foreach (string pl in lst)
+                {
+                    //listView1.Items.Add(pl);
+                    listView1.Items.Add(new ListViewItem(new string[] { "HI"}));
+
+                }
+                */
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> lst = new List<string>();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Files:");
+        }
     }
 }
